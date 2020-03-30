36c60e7c791befc0eac28c35f4a1f7de5c41a479
danieldagot <danieldagot@gmail.com>
2020-03-30 15:46:43 +0300

    Updates

---
+++ precache-manifest.e351f3acfe8b569903660a0c40e519c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ddbbbf86ef6751ea1e5791f65428172",
+    "url": "/zoom_mockup/index.html"
+  },
+  {
+    "revision": "bff027002a2a237ea0d9",
+    "url": "/zoom_mockup/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c480e1f6f0ef523350db",
+    "url": "/zoom_mockup/static/css/main.9116f83b.chunk.css"
+  },
+  {
+    "revision": "bff027002a2a237ea0d9",
+    "url": "/zoom_mockup/static/js/2.852950f0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/zoom_mockup/static/js/2.852950f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c480e1f6f0ef523350db",
+    "url": "/zoom_mockup/static/js/main.8a2e533c.chunk.js"
+  },
+  {
+    "revision": "3421a4b1c631684bef0f",
+    "url": "/zoom_mockup/static/js/runtime-main.01ca4626.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zoom_mockup/static/media/logo.5d5d9eef.svg"
+  }
+]);
